dc1e2eb8cc39f9a01edf704bd63e4bb373b346b6
David Glymph <Woozl@users.noreply.github.com>
2023-12-04 00:46:06 -0500

    day 4 pt 1

--- challenges/day-04/part-01.rs
+++ challenges/day-04/part-01.rs
@@ -1,10 +1,38 @@
-use std::{fs, path::Path};
+use std::{collections::HashSet, fs, path::Path};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = fs::read_to_string(Path::new("./challenges/day-04/input.txt"))?;
 
-    println!("Hello from day 4 part 1");
-    println!("Input file: {}", &input);
+    let mut card_pile_value: usize = 0;
+    for line in input.lines() {
+        let mut nums: Vec<Vec<usize>> = line
+            .split(":")
+            .skip(1)
+            .next()
+            .unwrap()
+            .split("|")
+            .map(|nums_str| {
+                nums_str
+                    .split_whitespace()
+                    .map(|num| num.parse::<usize>().unwrap())
+                    .collect()
+            })
+            .collect();
+        let (winning, scratched) = (nums.remove(0), nums.remove(0));
+
+        let winning_set: HashSet<usize> = HashSet::from_iter(winning);
+
+        let num_matching = scratched
+            .iter()
+            .filter(|num| winning_set.contains(num))
+            .count() as u32;
+
+        if num_matching > 0 {
+            card_pile_value += usize::pow(2, num_matching - 1);
+        }
+    }
+
+    println!("Sum: {card_pile_value}");
 
     Ok(())
 }
