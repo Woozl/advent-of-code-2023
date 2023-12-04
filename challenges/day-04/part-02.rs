d392e78aff16a876f1e091a8834d252c0051ea6e
David Glymph <Woozl@users.noreply.github.com>
2023-12-04 17:56:47 -0500

    day 4 pt 2

--- challenges/day-04/part-02.rs
+++ challenges/day-04/part-02.rs
@@ -1,10 +1,41 @@
-use std::{fs, path::Path};
+use std::{collections::HashSet, fs, path::Path};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = fs::read_to_string(Path::new("./challenges/day-04/input.txt"))?;
 
-    println!("Hello from day 4 part 2");
-    println!("Input file: {}", &input);
+    // copies of each card #, starts with 1 copy of each
+    let mut card_copies = vec![1; input.lines().count()];
+
+    for (card_number, line) in input.lines().enumerate() {
+        let mut nums: Vec<Vec<usize>> = line
+            .split(":")
+            .skip(1)
+            .next()
+            .unwrap()
+            .split("|")
+            .map(|nums_str| {
+                nums_str
+                    .split_whitespace()
+                    .map(|num| num.parse::<usize>().unwrap())
+                    .collect()
+            })
+            .collect();
+        let (winning, scratched) = (nums.remove(0), nums.remove(0));
+
+        let winning_set: HashSet<usize> = HashSet::from_iter(winning);
+
+        let card_points = scratched
+            .iter()
+            .filter(|num| winning_set.contains(num))
+            .count();
+
+        let copies_of_current_card = card_copies[card_number];
+        for i in (card_number + 1)..=(card_number + card_points) {
+            card_copies[i] += copies_of_current_card
+        }
+    }
+
+    println!("Sum: {}", card_copies.iter().fold(0, |acc, c| acc + c));
 
     Ok(())
 }
