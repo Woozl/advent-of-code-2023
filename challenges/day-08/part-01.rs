a11320a2c6edc59032cac3fb81bfcfa0d8192475
David Glymph <Woozl@users.noreply.github.com>
2023-12-08 02:06:09 -0500

    day 8 pt 1

--- challenges/day-08/part-01.rs
+++ challenges/day-08/part-01.rs
@@ -1,10 +1,35 @@
-use std::{fs, path::Path};
+use std::{collections::HashMap, fs, path::Path};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = fs::read_to_string(Path::new("./challenges/day-08/input.txt"))?;
 
-    println!("Hello from day 8 part 1");
-    println!("Input file: {}", &input);
+    let mut map: HashMap<String, (String, String)> = HashMap::new();
+    let turn_string = input.lines().take(1).collect::<String>();
+
+    // populate the map
+    for line in input.lines().skip(2) {
+        let split_line: String = line
+            .replace(" = (", " ")
+            .replace(", ", " ")
+            .replace(")", "");
+        let mut l: Vec<String> = split_line
+            .split_ascii_whitespace()
+            .map(|s| s.to_string())
+            .collect();
+        map.insert(l.remove(0), (l.remove(0), l.remove(0)));
+    }
+
+    let mut current_location = String::from("AAA");
+    let mut num_steps: usize = 0;
+    let mut turns = turn_string.chars().cycle();
+    while current_location != String::from("ZZZ") {
+        let crossroad = map.get(&current_location).unwrap();
+        let turn = turns.next().unwrap();
+        current_location = if turn == 'L' { crossroad.0.clone() } else { crossroad.1.clone() };
+        num_steps += 1;
+    }
+    
+    println!("Number of steps: {num_steps}");
 
     Ok(())
 }
