4c25edcf6cc1069f034adfd09d62e88a8e7ce07d
David Glymph <Woozl@users.noreply.github.com>
2023-12-16 16:41:45 -0500

    incomplete pt 12 parsing

--- challenges/day-12/part-01.rs
+++ challenges/day-12/part-01.rs
@@ -3,8 +3,19 @@ use std::{fs, path::Path};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = fs::read_to_string(Path::new("./challenges/day-12/input.txt"))?;
 
-    println!("Hello from day 12 part 1");
-    println!("Input file: {}", &input);
+    let row = input.lines().map(|l| {
+        let mut split = l.split_ascii_whitespace();
+        let springs = split.next().unwrap();
+        let broken_ranges: Vec<usize> = split
+            .next()
+            .unwrap()
+            .split(',')
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect();
+        (springs, broken_ranges)
+    });
+
+    row.for_each(| v | println!("{:?}", v));
 
     Ok(())
 }
